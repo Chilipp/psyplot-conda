dee18e998cdadcc670934bc1fd585d66bddd39ab
Chilipp <p.sommer@posteo.de>
2017-04-08 10:05:53 +0200

    Use git submodule foreach to update submodules

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,7 +1,4 @@
 # Update submodules to the latest version from github
 
-for PKG in psyplot psyplot-gui psy-simple psy-maps psy-reg; do
-    cd $PKG
-    git pull origin `git rev-parse --abbrev-ref HEAD`
-    cd ../
-done
+git submodule foreach 'git pull origin `git rev-parse --abbrev-ref HEAD`'
+
