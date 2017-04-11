901ab118be00b25b5023285c8eedfce3b6978b6b
Chilipp <p.sommer@posteo.de>
2017-04-11 22:13:27 +0200

    Install psyplot-conda in powershell

---
+++ ci/install_psyplot.ps1
@@ -0,0 +1,37 @@
+# script to install psyplot-conda under Windows
+
+
+function InstallPsyplot ($architecture, $python_home) {
+    Write-Host "Installing psyplot-conda for" $architecture "bit architecture to" $python_home
+    if (Test-Path $psyplot_home) {
+        Write-Host $psyplot_home "already exists, skipping."
+        return $false
+    }
+    if ($architecture -match "32") {
+        $platform_suffix = "x86"
+    } else {
+        $platform_suffix = "x86_64"
+    }
+    $basedir = $pwd.Path + "\"
+    $filename = "psyplot-conda-latest-Windows-" + $platform_suffix + ".exe"
+    $filepath = $basedir + $filename
+    Write-Host "Installing" $filepath "to" $python_home
+    $install_log = $python_home + ".log"
+    $args = "/S /D=$python_home"
+    Write-Host $filepath $args
+    Start-Process -FilePath $filepath -ArgumentList $args -Wait -Passthru
+    if (Test-Path $python_home) {
+        Write-Host "Psyplot ($architecture) installation complete"
+    } else {
+        Write-Host "Failed to install Python in $python_home"
+        Get-Content -Path $install_log
+        Exit 1
+    }
+}
+
+
+function main () {
+    InstallPsyplot $env:PYTHON_ARCH $env:PSYPLOT_PYTHON
+}
+
+main
