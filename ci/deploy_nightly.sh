1866b4f0772723f2f4172770cf78c906d6036ae6
Chilipp <p.sommer@posteo.de>
2017-05-12 05:26:32 +0200

    Minor bug fix for deploy [skip appveyor]

--- ci/deploy_nightly.sh
+++ ci/deploy_nightly.sh
@@ -4,4 +4,4 @@
 #
 #     bash deploy_nightly user:password filename
 
-url -X PUT -u $1 "https://drive.switch.ch/remote.php/webdav/psyplot-conda/$TRAVIS_BRANCH/$2" --data-binary @"$2"
+curl -X PUT -u $1 "https://drive.switch.ch/remote.php/webdav/psyplot-conda/$TRAVIS_BRANCH/$2" --data-binary @"$2"
