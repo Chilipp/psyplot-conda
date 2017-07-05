d20aea1dda027d070c4d6f95f6a3b5f38a3344b5
Chilipp <p.sommer@posteo.de>
2017-07-05 21:19:04 +0200

    Minor bug fix

--- constructor-files/post_win.bat
+++ constructor-files/post_win.bat
@@ -1,6 +1,6 @@
 :: Install mkl if numpy cannot be imported
 %PREFIX%\python.exe -c "import numpy" || (
-    %PREFIX%\Scripts\conda.exe -c conda-forge -y mkl
+    %PREFIX%\Scripts\conda.exe install -c conda-forge -y mkl
     ) || (
     ECHO "Could not install mkl. Run 'conda install -c conda-forge mkl'"
     )
