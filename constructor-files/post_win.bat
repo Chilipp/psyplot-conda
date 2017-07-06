6854c729cc039aaaba787143735c6f73bf872807
Chilipp <p.sommer@posteo.de>
2017-07-06 14:51:35 +0200

    Do everything in one line

--- constructor-files/post_win.bat
+++ constructor-files/post_win.bat
@@ -1,8 +1,2 @@
 :: Install mkl if numpy cannot be imported
-ECHO "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root
-call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root
-call "%PREFIX%\python.exe" -c "import numpy" || (
-    call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root
-    ) || (
-    ECHO "Could not install mkl. Run 'conda install -c conda-forge mkl'"
-    )
+call "%PREFIX%\python.exe" -c "import numpy" || call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root || ECHO "Could not install mkl. Run 'conda install -c conda-forge mkl'"
