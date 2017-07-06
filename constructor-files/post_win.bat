a6b9c1a5fc71044ce393cec32a11b2e69db77760
Chilipp <p.sommer@posteo.de>
2017-07-06 07:28:04 +0200

    Use 'call' instead of 'start' in post install script

--- constructor-files/post_win.bat
+++ constructor-files/post_win.bat
@@ -1,6 +1,6 @@
 :: Install mkl if numpy cannot be imported
-start "" "%PREFIX%\python.exe" -c "import numpy" || (
-    start "" "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl
+call "%PREFIX%\python.exe" -c "import numpy" || (
+    call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl
     ) || (
     ECHO "Could not install mkl. Run 'conda install -c conda-forge mkl'"
     )
