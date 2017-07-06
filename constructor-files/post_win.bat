7df8057763d2c7f18147e9dbfdaa3f4b4e1cfb07
Chilipp <p.sommer@posteo.de>
2017-07-06 14:33:32 +0200

    Another try to install mkl

--- constructor-files/post_win.bat
+++ constructor-files/post_win.bat
@@ -1,6 +1,8 @@
 :: Install mkl if numpy cannot be imported
+ECHO "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root
+call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root
 call "%PREFIX%\python.exe" -c "import numpy" || (
-    call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl
+    call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root
     ) || (
     ECHO "Could not install mkl. Run 'conda install -c conda-forge mkl'"
     )
