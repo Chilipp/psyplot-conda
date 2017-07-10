2195322fbef8d671f80bced4132e8bdd7e5e3935
Chilipp <p.sommer@posteo.de>
2017-07-10 17:21:55 +0200

    Fixed call syntax

--- constructor-files/post_win.bat
+++ constructor-files/post_win.bat
@@ -1,2 +1,3 @@
 :: Install mkl if numpy cannot be imported
-call "%PREFIX%\python.exe" -c "import numpy" || call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root || ECHO "Could not install mkl. Run 'conda install -c conda-forge mkl'"
+call "%PREFIX%\python.exe" -c "import numpy" && EXIT /B 0 || ECHO "Installing mkl to fix numpy"
+call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root || ECHO "Could not install mkl. Run 'conda install -c conda-forge mkl'"
