35c425f6741bcbbee87798447c361cb367799fff
Chilipp <p.sommer@posteo.de>
2017-08-08 23:59:41 +0200

    Added scripts for package installer on OS X

---
+++ create_package.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+cp -r osx-app work
+
+cd work
+
+# insert versions
+sed -i "s/0.01/$PKG_VERSION/" Psyplot.app/psyplotapp/Contents/Info.plist
+sed -i "s/0.01/$PKG_VERSION/" distribution.xml
+chmod +x scripts/postinstall
+
+# extract the app
+tar xjvf `conda build ../recipes/psyplot-gui --output | tail -n 1` psyplotapp
+mv psyplotapp Psyplot.app
+
+# convert intro, lices and conclusions to html
+mkdir resources || :
+pandoc -f markdown ../LICENSE -o resources/license.html
+pandoc psyplot-conda/intro.rst -o resources/welcome.html
+pandoc conclusion.rst -o resources/conclusion.html
+
+cp ../psyplot-conda-*MacOSX-*.sh scripts/psyplot-conda-install.sh
+
+# build the psyplot package
+pkgbuild --component Psyplot.app \
+         --scripts scripts \
+         --version $PKG_VERSION \
+         --ownership recommended \
+         Psyplot.pkg
+
+# build the final package
+productbuild --distribution distribution.xml \
+             --resources resources \
+             --package-path Psyplot.pkg \
+             --version $PKG_VERSION \
+             ../Psyplot.pkg
+
+rm Psyplot.pkg
---
+++ osx_app/conclusion.rst
@@ -0,0 +1,8 @@
+Congratulations! You're done!
+
+Start psyplot by clicking on the app. To update any of the packages,
+run::
+
+    !conda update <pkg-name>
+
+in the console of the app.
