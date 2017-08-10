5059cbf5134eed06bdeb9b12cc7809c598641252
Chilipp <p.sommer@posteo.de>
2017-08-10 11:08:52 +0200

    Specify install-location in pkgbuild call

--- create_package.sh
+++ create_package.sh
@@ -32,6 +32,7 @@ pkgbuild --component Psyplot.app \
          --scripts scripts \
          --version $PKG_VERSION \
          --ownership recommended \
+         --install-location /Applications
          Psyplot.pkg
 
 # build the final package
