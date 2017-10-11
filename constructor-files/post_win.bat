7f4be010c0e6ae5ff7265632e35efb5742479d0d
Chilipp <p.sommer@posteo.de>
2017-10-12 00:44:48 +0200

    Do not install mkl in post script on windows

--- constructor-files/post_win.bat
+++ constructor-files/post_win.bat
@@ -1,7 +1,3 @@
-:: Install mkl if numpy cannot be imported
-call "%PREFIX%\python.exe" -c "import numpy" && EXIT /B 0 || ECHO "Installing mkl to fix numpy"
-call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root || ECHO "Could not install mkl. Run 'conda install -c conda-forge mkl'"
-
 :: Install pyshp, alabaster and dask if they cannot be imported
 call "%PREFIX%\python.exe" -c "import shapefile, alabaster, dask" && EXIT /B 0 || ECHO "Installing pyshp, alabaster and dask"
 call "%PREFIX%\Scripts\conda.exe" install -y "%PREFIX%\pkgs\alabaster-ALABASTERVERSION.tar.bz2" -n root || ECHO ""
