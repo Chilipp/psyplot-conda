c0ea81e8983229207aac4207bfe67cd4dd4ee5a7
Chilipp <p.sommer@posteo.de>
2017-10-12 00:28:25 +0200

    Minor changes

--- constructor-files/post_win.bat
+++ constructor-files/post_win.bat
@@ -4,5 +4,6 @@ call "%PREFIX%\Scripts\conda.exe" install -c conda-forge -y mkl -n root || ECHO 
 
 :: Install pyshp, alabaster and dask if they cannot be imported
 call "%PREFIX%\python.exe" -c "import shapefile, alabaster, dask" && EXIT /B 0 || ECHO "Installing pyshp, alabaster and dask"
-call "%PREFIX%\Scripts\conda.exe" install -y "%PREFIX%\pkgs\pyshp-PYSHPVERSION.tar.bz2" "%PREFIX%\pkgs\alabaster-ALABASTERVERSION.tar.bz2" -n root || ECHO ""
+call "%PREFIX%\Scripts\conda.exe" install -y "%PREFIX%\pkgs\alabaster-ALABASTERVERSION.tar.bz2" -n root || ECHO ""
+call "%PREFIX%\Scripts\conda.exe" install -y "%PREFIX%\pkgs\pyshp-PYSHPVERSION.tar.bz2" -n root || ECHO ""
 call "%PREFIX%\Scripts\conda.exe" install -y "%PREFIX%\pkgs\dask-DASKVERSION.tar.bz2" -n root || ECHO ""
